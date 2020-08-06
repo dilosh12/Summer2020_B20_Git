52428564383d376d2a8ab8a177720fa29752250a
Diloro A <68902194+dilosh12@users.noreply.github.com>
2020-08-05 21:35:18 -0400

    Update GitFun.java

--- src/Utility/GitFun.java
+++ src/Utility/GitFun.java
@@ -3,5 +3,6 @@ package Utility;
 public class GitFun {
     public static void main(String[] args) {
         System.out.println("Everything is fun");
+        System.out.println("Everything is fun! YAY!!!");
     }
 }
